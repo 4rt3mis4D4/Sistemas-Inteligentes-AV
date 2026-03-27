8dc53503fc801288b786cf39b70a199286d51726
Artemis <130113421+4rt3mis4D4@users.noreply.github.com>
2026-03-27 12:06:33 -0300

    Add normalization script for new data instance

---
+++ Conteudo/Normalização/normalizacao_nova_instancia.py
@@ -0,0 +1,15 @@
+# Recebe um novo valor numero e normaliza com o modelo original
+import pickle
+
+# Abrir o modelo normalizador original
+scaler_model = pickle.load(open('scaler1.pkl', 'rb'))
+
+novo_dado = [[2000]]
+novo_dado_norm = scaler_model.transform(novo_dado)
+
+print(novo_dado)
+print(novo_dado_norm)
+
+# Desnormalizar 
+dado_revertido = scaler_model.inverse_transform(novo_dado_norm)
+print(dado_revertido)
